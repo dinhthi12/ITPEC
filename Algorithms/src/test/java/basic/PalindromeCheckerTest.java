d494c7b137266f72f973c32a293436a0d428cf96
dinhthi12 <thi884641@gmail.com>
2024-06-10 14:10:13 +0700

    [Update] commit

--- Algorithms/src/test/java/basic/PalindromeCheckerTest.java
+++ Algorithms/src/test/java/basic/PalindromeCheckerTest.java
@@ -2,7 +2,8 @@ package basic;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PalindromeCheckerTest {
     @Test
