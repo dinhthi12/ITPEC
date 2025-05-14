f52716b79961a7ff39072b7c5db2604e581181b7
dinhthi12 <thi884641@gmail.com>
2025-05-14 10:44:06 +0700

    Update full course days

---
+++ FullCourse/VariablesAndBasicDataTypes/index.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main()
+{
+  // integer (whole number)
+  int age = 21;
+  int year = 2023;
+  int days = 7.5;
+
+  // double (number including decimal)
+  double price = 10.99;
+  double gpa = 2.5;
+  double temperature = 25.1;
+
+  // single character
+  char grade = 'A';
+  char initial = 'B';
+  char dollarSign = '$';
+
+  // boolean (true or false)
+  bool student = false;
+  bool power = true;
+
+  // string (object that represents a sequence of text)
+  std::string name = "Bro";
+  std::string day = "Monday";
+  std::string food = "Pizza";
+  std::string address = "123 Fake St.";
+
+  std::cout << "Hello " << name << '\n';
+  std::cout << "Your are " << age << " Year old";
+
+  return 0;
+}
